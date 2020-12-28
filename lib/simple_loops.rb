664e901ffccfbb3d4b3e6b951ed8ef13a908e077
Jacob Lepler <lepler.jacob@gmail.com>
2020-12-28 17:25:14 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,5 +1,5 @@
 # Write your methods here
-four_methods = ["loop_message_five_times", "loop_message_n_times", "output_array", "return_string_array"]
+# four_methods = ["loop_message_five_times", "loop_message_n_times", "output_array", "return_string_array"]
 
 # Method takes one string argument and puts string 5 times
 def loop_message_five_times(string)
